37d472f86ef2baa4f317804b2d15eb5fbc9df4e2
NgocPMT <np1552005@gmail.com>
2025-09-04 17:29:12 +0700

    style the homepage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,20 +2,33 @@ import { Link } from "react-router";
 
 const Home = () => {
   return (
-    <div>
-      <header className="flex justify-between">
-        <Link to="/">Medium</Link>
-        <nav>
-          <button>Get started</button>
-        </nav>
+    <div className="grid grid-rows-[auto_1fr_auto] min-h-dvh text-black">
+      <header>
+        <div className="flex justify-between items-center p-5 max-w-7xl md:px-12 lg:mx-auto">
+          <Link to="/" className="font-lora font-bold text-3xl">
+            Easium
+          </Link>
+          <nav className="flex items-center gap-3">
+            <Link to="sign-in" className="max-sm:hidden mr-3">
+              Sign in
+            </Link>
+            <button className="btn">Get started</button>
+          </nav>
+        </div>
+        <hr />
       </header>
-      <hr />
-      <section>
-        <h1>Human stories &amp; ideas</h1>
-        <p>A place to read, write, and deepen your understanding</p>
-        <button>Start reading</button>
+      <section className="flex flex-col justify-center items-start px-5 pt-16 gap-8  md:px-12 max-w-7xl lg:mx-auto">
+        <h1 className="font-lora text-7xl sm:mr-64 md:mr-96 md:text-8xl">
+          Human stories &amp; ideas
+        </h1>
+        <p className="text-2xl">
+          A place to read, write, and deepen your understanding
+        </p>
+        <button className="btn px-11 text-lg  hover:opacity-80 transition-all">
+          Start reading
+        </button>
       </section>
-      <footer>
+      <footer className="bg-black text-white text-xs flex gap-4 py-6.5 px-5 md:px-12 lg:justify-center">
         <Link to="#">About</Link>
         <Link to="#">Help</Link>
         <Link to="#">Terms</Link>
