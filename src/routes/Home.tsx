801e2feea8e6fe31af48041d3c1544e316ca5c39
NgocPMT <np1552005@gmail.com>
2025-09-04 15:37:44 +0700

    implement the HTML skeleton for the home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router";
+
 const Home = () => {
-  return <p>Hello World</p>;
+  return (
+    <div>
+      <header className="flex justify-between">
+        <Link to="/">Medium</Link>
+        <nav>
+          <button>Get started</button>
+        </nav>
+      </header>
+      <hr />
+      <section>
+        <h1>Human stories &amp; ideas</h1>
+        <p>A place to read, write, and deepen your understanding</p>
+        <button>Start reading</button>
+      </section>
+      <footer>
+        <Link to="#">About</Link>
+        <Link to="#">Help</Link>
+        <Link to="#">Terms</Link>
+        <Link to="#">Privacy</Link>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
